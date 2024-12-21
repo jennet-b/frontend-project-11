ad9a2961d8b5897f484aab9d0e4276140a7e30b3
Dzhennet Berdieva <berdievajennet@yandex.ru>
2024-12-21 14:09:09 +0300

    fix step 1

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/jennet-b/frontend-project-11/actions/workflows/hexlet-check.yml/badge.svg)](https://github.com/jennet-b/frontend-project-11/actions)
 
 [![Maintainability](https://api.codeclimate.com/v1/badges/ebfbf0dc3bce4713f71d/maintainability)](https://codeclimate.com/github/jennet-b/frontend-project-11/maintainability)
+
+Deploy: https://frontend-project-11-red-nine.vercel.app/
--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+import path from 'path';
 import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
 
 export default {
   mode: process.env.NODE_ENV || 'development',
@@ -14,10 +16,18 @@ export default {
           },
         },
       },
-      { test: /\.css$/, use: ['style-loader', 'css-loader', 'postcss-loader'] },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader'],
+      },
       {
         test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader'],
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+          'postcss-loader',
+        ],
       },
       {
         test: /\.woff2?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
@@ -33,8 +43,13 @@ export default {
     new HtmlWebpackPlugin({
       template: 'index.html',
     }),
+    new MiniCssExtractPlugin({
+      filename: 'main.css',
+    }),
   ],
   output: {
     clean: true,
+    path: path.resolve('dist'),
+    filename: 'main.js',
   },
 };
